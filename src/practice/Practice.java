b5fa5b6ff465d3684539d47c45058b43cc58b8f6
Edor Kacerja <edorkacerja@gmail.com>
2016-03-31 18:25:40 +0300

    solved difficult task, to find palindrome which is the square of the largest 3 digit number

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -7,38 +7,79 @@ package practice;
 
 import java.util.ArrayList;
 import java.util.List;
+import static oracle.jrockit.jfr.events.Bits.length;
 
 /**
  *
  * @author AcerPC
  */
 public class Practice {
-
+            
+     
     
     public static void main(String[] args) {
-            
-            
-
-//            The prime factors of 13195 are 5, 7, 13 and 29.
+        
+        int n = 999;
+            int res = n*n;
+            String resultString = Integer.toString(res);
+            int length = resultString.length();
+        
+        int i = 0;
+        
+        //9801
+        
+        for (int edor = 0; edor <(length/2)+1; edor++){
+            if(resultString.charAt(i) == resultString.charAt(length-1)){
+                i++;
+                length--;
+                System.out.println(" palindrome");
+            }else{
+                System.out.println("not palindrome");
+                n--;
+                res= n*n;
+                i=0;
+                resultString = Integer.toString(res);
+                length = resultString.length();
+                edor = -1;
+                
+            }
+        }
+        
+        System.out.println(n);
+        System.out.println(res);
+        
+        
+        
+        
+        boolean isPalindrome = false;
+        
+        
+//        while (isPalindrome == false){
+//            
+//            result = n*n;
+//            outerloop: for(int v=1; v<(length/2)+1; v++){
+//                
+//            
+//                if( resultString.charAt(i) == resultString.charAt(length-1) ){
+//                    i++;
+//                    length--;
+//                    
 //
-//What is the largest prime factor of the number 600851475143 ?
-            List<Integer> edorsArray = new ArrayList<Integer>();
-            long result = 600851475143L;
-            int i = 2; 
-            
-            while (result != 1){
-                if (result%i == 0){
-                    result = result/i;
-                    edorsArray.add(i);
-                }else{
-                    i++;
-                }
-            }
-            
-            System.out.print(edorsArray);
-            for (int j=0; j<edorsArray.size();j++){
-                System.out.println(edorsArray.get(j));
-            }
+//                }else{
+//                    n--;
+                      
+//                    break outerloop;
+//                }
+//                
+//                System.out.print(result);
+//                isPalindrome = true;
+//                
+//                
+//            }
+//
+//        }
+        
+        
 
     }
     
