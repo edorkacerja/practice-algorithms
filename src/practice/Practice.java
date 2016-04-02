7409d551280a6f0c364221936b217c2bee389cc7
Edor Kacerja <edorkacerja@gmail.com>
2016-04-02 14:31:33 +0300

    next optimized

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -22,29 +22,21 @@ static List<String> myArray = new ArrayList<String>();
         
         public static void main (String[] args)
    {		
-       int i =0;
-       int num =0;
-       //Empty String
-       String  primeNumbers = "";
-       List<Integer> myArray = new ArrayList<Integer>();
-       for (i = 1; i <= 110012; i++)         
-       { 		  	  
-          int counter=0; 	  
-          for(num =i; num>=1; num--)
-	  {
-             if(i%num==0)
-	     {
- 		counter = counter + 1;
-	     }
-	  }
-	  if (counter ==2)
-	  {
-	     //Appended the Prime number to the String
-	     myArray.add(i);
-	  }	
-       }	
-       System.out.println("Prime numbers from 1 to 100 are :");
-       System.out.println(myArray.get(10000));
+       
+       int SS = 0;
+       int SoS = 0;
+       int res = 0;
+       
+       for (int n=1; n<=100; n++){
+           SS += n*n;
+           SoS += n;
+       }
+       
+       res = SoS*SoS - SS;
+       
+       System.out.println(res);
+       
+       
    }
     
     
