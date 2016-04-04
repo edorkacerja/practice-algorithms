15f0c8873ab590240481c0d2528590e448f1dd01
Edor Kacerja <edorkacerja@gmail.com>
2016-04-04 15:04:12 +0300

    problem 14

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -14,34 +14,37 @@ package practice;
  */
 public class Practice {
     
-    
-        
+        public static int biggest = 0;
        
 	
 	public static void main(String[] args)
 	{
             int count = 0;
             int bestNumber = 0;
-            int biggest = 0;
             
             
-            for(int number=10; number<1000000; number++){
+            
+            for(int number=999999; number>13; number--){
                 count = 0;
-                biggest = 0;
+                
                 bestNumber = number;
                 
-                while(number != 1){
-                    if (number %2 == 0){
-                        number = number / 2;
-                    }else{
-                        number = 3*number -1;
+                while(bestNumber != 1){
+                    
+                    if (bestNumber %2 == 0){
+                        bestNumber = bestNumber / 2;
+                    }else if(bestNumber %2 == 1){
+                        bestNumber = 3*bestNumber +1;
                     }
                     count++;
+//                    System.out.println(bestNumber);
                 }
-                
+
                 if(count>=biggest){
                     biggest = count;
-                    System.out.println(bestNumber);
+                    System.out.println(biggest);
+                    count = 0;
+                    System.out.println(number);
                 }
                 
             }
