e4738d4d2d922ba6a2dd1f030558bb35b20cf362
Edor Kacerja <edorkacerja@gmail.com>
2016-04-10 00:41:32 +0300

    22, nice one actually

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -5,8 +5,18 @@
  */
 package practice;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 
@@ -17,45 +27,59 @@ import java.util.LinkedList;
  */
 public class Practice {
     
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
 	{
             
-            //for i : 1to1000
-            //call function divisorSum(i)
-                //for j : 1to1000
-                //call function divisorSum(j)
-                //if 1=2
-                    //sum= sum + i + j
-            int bigSum = 0;
-//            for(int i = 1; i <=1000; i++){
-//                int sumOfDiv = divisorSum(i);
-//                int sumOfDiv2 = divisorSum(sumOfDiv);
-//                if(sumOfDiv==sumOfDiv2){
-//                    bigSum =bigSum + sumOfDiv + sumOfDiv2;
-//                }
-//            }
-//            
-//            System.out.println(bigSum);
+            
+            String[] names;
+            
+            long total = 0;
+            try{
+            String line;
+                InputStream fis = new FileInputStream("names.txt");
+                InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+                BufferedReader br = new BufferedReader(isr);
+            {
+                while ((line = br.readLine()) != null) {
 
-System.out.println(divisorSum(220));
-        }
-        
-        //function which returns the sum of the divisors of a number
-            //for k: number-1 to 2
-                //if number%k == 0
-                    //sumDiv += k
-        
-        public static int divisorSum (int number){
-            int sumOfDivisors = 0;
-            for(int k = number-1; k>=2; k--){
-                if(number%k == 0){
-                    sumOfDivisors += k;
+                    line = line.replace("\"", "");
+                    names = line.split(",");
+                    
+                    Arrays.sort(names);
+                    for (int i=0; i<names.length; i++){
+                        int product =1;
+                        int integerValue = 0;
+                        int totalIntegerValue=0;
+                        //calculate integer value
+                        char[] ch  = names[i].toCharArray();
+                        for(char c : ch)
+                        {
+                            
+                            int temp = (int)c;
+                            int temp_integer = 64; //for upper case
+                            if(temp<=90 & temp>=65){
+                                integerValue = temp - temp_integer;
+                                totalIntegerValue += integerValue;
+                            }
+                        }
+                        //calculate product = value*position
+                        product = totalIntegerValue * (i+1);
+                        //add product to total
+                        total+=product;
+                        System.out.println(total);
+                    }
                 }
             }
-            return sumOfDivisors;
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+            
+            
+            
+            
+            
         }
     
     
 }
-
-
