ef686a1e560163b04545bc869f9af5aa40149f13
Edor Kacerja <edorkacerja@gmail.com>
2016-04-21 18:44:48 +0300

    Stack impl

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -7,6 +7,7 @@ package practice;
 
 import java.io.BufferedReader;
 import java.util.Random;
+import java.util.Stack;
 import java.util.Vector;
 
 
@@ -16,85 +17,79 @@ import java.util.Vector;
  */
 
 
-public class Practice {
+public class Practice extends Stack{
     
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private Node top = null;
+    private int numElems = 0;
+    
+    
+    public void push(Object itemObj){
+        Node newNode = new Node(itemObj, top);
+        top = newNode;
+        numElems++;
+    }
+    
+    public void pop(){
+        if(isEmpty()){
+            throw new NoSuchElementException("Stack is empth");
+        }else{
+            Node oldNode = top;
+            top = top.next;
+            numElems--;
+            oldNode = null;
+        }
+    }
    
-    
-    public static void main(String[] args){
-        
-        
-        Random r = new Random(); 
-        double randomDouble;
-        double[][] myOtherArray = new double[20][6];
-            for (int i=0; i<20; i++){
-            for (int j = 0; j<6; j++){
-                randomDouble = r.nextDouble()*100;
-                myOtherArray[i][j] = randomDouble;
-            }
+    public void makeEmpty(){
+        while(top != null){
+            Node previous = top;
+            top = top.next;
+            previous = null;
         }
+        numElems = 0;
+    }
+    
+    public Object top(){
+        if(isEmpty()){
+            throw new NoSuchElementException("Stack is empty");
+        }else{
+            return top.item;
+        }
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+    
+    public int size(){
+        return numElems;
+    }
+    
+    private class Node {
+        private Object item;
+        private Node next;
+        
+        private Node(Object element, Node nodeLink){
+            item = element;
+            next = nodeLink;
+        }
+    }
+    
+   public static void main(String args[]) {
+      JavaApplication1 myLinkedStack = new JavaApplication1();
+      myLinkedStack.push(new)
+   }
         
         
         
-        
-        
-        TwoDimetionalArray edorsArray = new TwoDimetionalArray(myOtherArray, 20, 6);
-           
-        System.out.println(edorsArray.toString());
-        System.out.println(edorsArray.colAverage(4));
-        System.out.println(edorsArray.rowAverage(0));
-        System.out.println(edorsArray.overallAverage());
 
         
-        
-           
-            
-        }
-    
-    
     
 }
-
-class TwoDimetionalArray {
     
-    private double[][] data;
-    private int cols;
-    private int rows;
     
-    public TwoDimetionalArray(double[][] data, int rows, int cols){
-        this.data = data;
-        this.rows = rows;
-        this.cols = cols;
-    }
-    
-    public double rowAverage(int rowNumber){
-        double rowSum=0;
-        for(int i = 0; i < cols; i++){
-            rowSum += data[rowNumber][i];
-        }
-        
-        return rowSum/cols;
-    }
-    
-    public double colAverage(int colNumber){
-        double colSum=0;
-        for(int i = 0; i < rows; i++){
-            colSum += data[i][colNumber];
-        }
-        
-        return colSum/rows;
-    }
-    
-    public double overallAverage(){
-        double totalSum = 0;
-        
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                totalSum += data[i][j];
-            }
-        }
-        
-        return totalSum/(rows*cols);
-    }
-}
-
-
